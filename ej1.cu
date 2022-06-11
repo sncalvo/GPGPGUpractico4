0cbe1f285a95c52b6318351f6babbaa8c8a6bfbd
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-06-11 17:08:59 -0300

    Fix bug

--- ej1.cu
+++ ej1.cu
@@ -86,8 +86,8 @@ int parte_2(int length, unsigned int size, int *message, int *occurenses)
 	cudaMalloc((void**)&d_occurenses, M * sizeof(int));
 	cudaMemset(d_occurenses, 0, M * sizeof(int));
 
-	int block_dim = BLOCK_SIZE
- 	int grid_dim = (size + blockSize - 1) / blockSize;
+	int block_dim = BLOCK_SIZE;
+ 	int grid_dim = (size + block_dim - 1) / block_dim;
 
 	decrypt_kernel<<<grid_dim, block_dim>>>(d_message, length);
 	count_occurences<<<grid_dim, block_dim, BLOCK_SIZE * sizeof(int)>>>(d_message, d_occurenses, length);
