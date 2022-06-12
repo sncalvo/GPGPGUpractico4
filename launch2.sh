6c1f173111c955a0aebfb8a5acafc97be2fdc0ce
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-06-11 22:09:15 -0300

    Add launch 2

---
+++ launch2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --job-name=mitrabajo
+#SBATCH --ntasks=1
+#SBATCH --mem=512
+#SBATCH --time=00:01:00
+
+#SBATCH --partition=besteffort
+
+#SBATCH --qos=besteffort_gpu
+
+#SBATCH --gres=gpu:1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=sncalvo5@gmail.com
+#SBATCH -o salida.out
+
+export PATH=$PATH:/usr/local/cuda/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
+
+nvcc ./ej2.cu -o ej2_sol
+
+./ej2_sol secreto.txt
+# nvprof --metrics gld_efficiency,gst_efficiency ./ej1_sol
+# nvprof ./ej1_sol
