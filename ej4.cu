53ca79c1933942f6a16821b06a8548fc84d36cc8
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-06-13 21:49:11 -0300

    Get output

--- ej4.cu
+++ ej4.cu
@@ -25,6 +25,28 @@ __global__ void sum_col_block(int *data, int length) {
 	data[idy*n+idx] = col_sum;
 }
 
+__global__ void sum_col_block_opt(int *data, int length) {
+	__shared__ int sh_tile[TSZ][TSZ];
+
+	int n = gridDim.x * blockDim.x;
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	int idy = blockIdx.y * blockDim.y + threadIdx.y;
+
+	sh_tile[threadIdx.y][threadIdx.x] = data[idy*n+idx];
+
+	__syncthreads();
+
+	int col_sum = sh_tile[threadIdx.x][threadIdx.y];
+
+	for (int i=16; i>0; i/=2)
+	{
+		if (threadIdx.x + i < TSZ)
+			col_sum += sh_tile[threadIdx.x+i][threadIdx.y];
+	}
+
+	data[idy*n+idx] = col_sum;
+}
+
 int main() {
 	int *data_host = (int*)malloc(sizeof(int)*DATA_SIZE*DATA_SIZE);
 	int *data;
@@ -41,6 +63,13 @@ int main() {
 	sum_col_block<<<dimGrid, dimBlock>>>(data, DATA_SIZE*DATA_SIZE);
 	cudaDeviceSynchronize();
 	cudaMemcpy(data_host, data, sizeof(int)*DATA_SIZE*DATA_SIZE, cudaMemcpyDeviceToHost);
+
+	for (int i=0; i<DATA_SIZE; i++) {
+		for (int j=0; j<DATA_SIZE; j++)
+			printf("%d ", data_host[i*DATA_SIZE+j]);
+
+		printf("\n");
+	}
 
 	cudaFree(data);
 
