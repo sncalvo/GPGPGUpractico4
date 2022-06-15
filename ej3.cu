46ba21e1da16431e0896ceaf0fd88de557e41340
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-06-15 19:46:14 -0300

    Improve 3

--- ej3.cu
+++ ej3.cu
@@ -4,30 +4,18 @@
 
 #include "cuda.h"
 
-// __global__ void generator(int num_points, int *points, int max) {
-//   int i = blockIdx.x * blockDim.x + threadIdx.x;
-
-//   if (i < num_points) {
-// 		// sudo random generator on device
-//     points[i] = max(i, max) >> 2 + i;
-//   }
-// }
-
-
 // se asume que el tamaño de perm es igual al del bloque
 // y que las premutaciones son válidas
 __global__ void block_perm(int *data, int *perm, int length) {
 	int off = blockIdx.x * blockDim.x;
 	__shared__ int shared_data[1024];
-	__shared__ int shared_perm[1024];
 
 	if (length < off + threadIdx.x) return;
 
-	shared_perm[threadIdx.x] = perm[threadIdx.x];
 	shared_data[threadIdx.x] = data[off];
 	__syncthreads();
 
-	data[off+threadIdx.x] = shared_data[shared_perm[threadIdx.x]];
+	data[off+threadIdx.x] = shared_data[perm[threadIdx.x]];
 }
 
 __global__ void block_perm_org(int * data, int *perm, int length) {
